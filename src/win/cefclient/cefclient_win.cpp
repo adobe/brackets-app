5155a935abddeff54599179a203a00fc2f718077
Randy Edmunds <redmunds@adobe.com>
2012-01-31 09:07:16 -0800

    interim brackets icons for windows -- needs final art

--- src/win/cefclient/cefclient_win.cpp
+++ src/win/cefclient/cefclient_win.cpp
@@ -175,12 +175,12 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 	wcex.cbClsExtra		= 0;
 	wcex.cbWndExtra		= 0;
 	wcex.hInstance		= hInstance;
-	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_CEFCLIENT));
+	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_BRACKETS));
 	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
 	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
 	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_CEFCLIENT);
 	wcex.lpszClassName	= szWindowClass;
-	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_BRACKETS_SMALL));
 
 	return RegisterClassEx(&wcex);
 }
