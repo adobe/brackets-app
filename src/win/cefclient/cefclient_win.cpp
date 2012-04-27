d85733dfa171b2f52fcdb1bdb027eb06708fa6b1
RaymondLim <rayblim@gmail.com>
2012-04-27 15:45:12 -0700

    Replacing one tab with spaces that I missed in previous commit

--- src/win/cefclient/cefclient_win.cpp
+++ src/win/cefclient/cefclient_win.cpp
@@ -424,7 +424,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         {
         case IDM_ABOUT:
             // Brackets: Delegate to JavaScript code to show an HTML version of the about box.
-        	if (browser.get())
+            if (browser.get())
                 BracketsShellAPI::DispatchShowAboutToBracketsJS(browser);
             return 0;
         case IDM_EXIT:
